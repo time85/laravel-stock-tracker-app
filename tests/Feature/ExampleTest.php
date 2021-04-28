5ca5e337be6682f3b9a3200addc7b0474af140c3
Thomas Imensek <thomas.imensek@gmail.com>
2021-04-28 15:22:38 +0200

    trying TDD Approach;

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,25 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product; 
+use App\Models\Retailer; 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_checks_stocks_for_products_as_retailers()
+    {
+        $switch = Product::create(['name' => 'Nintendo Switch (Neon)']);
+
+        $retailer = Retailer::create(['name' => 'Spar']);
+
+        $this->assertFalse($switch->inStock());
+        //$retailer->hasStock($switch);
+
     }
 }
